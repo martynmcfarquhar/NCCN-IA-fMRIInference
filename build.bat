9769ca3f40b20ed26f7dd778cfa030dd740291d2
Martyn McFarquhar <martynmcfarquhar@googlemail.com>
2024-11-14 13:56:48 +0000

    'updates'

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ Remove-Item -Force -Recurse -Path .\_build
 jupyter-book build .\
 copy spm152.html _build\html\spm152.html
 copy p-uncorr-001.html _build\html\p-uncorr-001.html
+copy p-fdr-05.html _build\html\p-fdr-05.html
 git add -A
 git commit -m 'updates'
 git push
