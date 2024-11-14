6a317cfb18a8d24cc6af09c0046e0f82fdca4e52
Martyn McFarquhar <martynmcfarquhar@googlemail.com>
2024-11-14 12:07:17 +0000

    'updates'

--- .ipynb_checkpoints/build-checkpoint.sh
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -2,6 +2,7 @@
 
 rm -rf ./_build
 jupyter-book build ./
+cp spm152.html _build/html/spm152.html
 git add -A
 git commit -m 'updates'
 git push
--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 Remove-Item -Force -Recurse -Path .\_build
 jupyter-book build .\
+copy spm152.html _build\html\spm152.html
 git add -A
 git commit -m 'updates'
 git push
