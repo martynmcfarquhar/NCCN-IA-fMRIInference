b42afe10114abd1ef82d3c8aab9bccbbac74c11d
Martyn McFarquhar <martynmcfarquhar@googlemail.com>
2024-11-14 13:59:16 +0000

    'updates'

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ jupyter-book build .\
 copy spm152.html _build\html\spm152.html
 copy p-uncorr-001.html _build\html\p-uncorr-001.html
 copy p-fdr-05.html _build\html\p-fdr-05.html
+copy p-fdr-topo-05.html _build\html\p-fdr-topo-05.html
 git add -A
 git commit -m 'updates'
 git push
