0b47cfb114652c0cde9eaddcc1d39b3dc4680b94
Martyn McFarquhar <martynmcfarquhar@googlemail.com>
2024-11-14 15:54:26 +0000

    'updates'

--- .ipynb_checkpoints/build-checkpoint.sh
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -3,6 +3,10 @@
 rm -rf ./_build
 jupyter-book build ./
 cp spm152.html _build/html/spm152.html
+cp p-uncorr-001.html _build/html/p-uncorr-001.html
+cp p-fdr-05.html _build/html/p-fdr-05.html
+cp p-fdr-topo-05.html _build/html/p-fdr-topo-05.html
+cp p-fwe-05.html _build/html/p-fwe-05.html
 git add -A
 git commit -m 'updates'
 git push
